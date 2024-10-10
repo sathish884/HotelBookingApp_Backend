44c527a0dec36e896438208bcabe0b774a770640
Sathish M. <M1037@eaglesoftware.com>
2024-10-10 19:23:52 +0530

    updates

--- models/User.js
+++ models/User.js
@@ -1,25 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const UserShema = new mongoose.Schema(
-//     {
-//         name: { type: String, required: true },
-//         email: { type: String, required: true },
-//         password: { type: String, required: true },
-//         confirmPassword: { type: String, required: true },
-//         otp: { type: String },
-//         otpExpiry: { type: Date },
-//         resetPasswordToken: { type: String },
-//         resetPasswordTokenExpiry: { type: Date },
-//         isActive: {
-//             type: Boolean,
-//             default: false,
-//         },
-//     },
-//     { timestamps: true }
-// );
-
-// module.exports = mongoose.model("Users", UserShema)
-
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
